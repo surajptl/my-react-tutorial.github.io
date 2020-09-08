ac1ee4b409d8e1cfda36f09bcf1e8c0dffd10f6a
Suraj Patel <suraj326len@gmail.com>
2020-09-08 09:49:10 +0530

    Updates

---
+++ precache-manifest.9b5c6fdaa8c49c9a8f109c056be7ac3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafcd1928888c308d74cbbaa36a45d45",
+    "url": "/surajptl/my-react-tutorial.github.io/index.html"
+  },
+  {
+    "revision": "b1eae7b07518335ce289",
+    "url": "/surajptl/my-react-tutorial.github.io/static/css/main.53574754.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/surajptl/my-react-tutorial.github.io/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/surajptl/my-react-tutorial.github.io/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1eae7b07518335ce289",
+    "url": "/surajptl/my-react-tutorial.github.io/static/js/main.85bfa427.chunk.js"
+  },
+  {
+    "revision": "bb2de36121f771b7f2c7",
+    "url": "/surajptl/my-react-tutorial.github.io/static/js/runtime-main.9bf5878a.js"
+  }
+]);
