3d2e5bbb6a5d6d3336ba671fee9776bb3a6462ab
Suraj Patel <suraj326len@gmail.com>
2020-09-08 09:24:19 +0530

    complete tutorial

---
+++ src/Table.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react'
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => 
+                    props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        )
+    })
+    // return (
+    //     <tbody>
+    //         <tr>
+    //             <td>Charlie</td>
+    //             <td>Janitor</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Mac</td>
+    //             <td>Bouncer</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Dee</td>
+    //             <td>Aspiring Actress</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Dennis</td>
+    //             <td>Bartender</td>
+    //         </tr>
+    //     </tbody>
+    // )
+    return <tbody>{rows}</tbody>
+}
+
+class Table extends Component {
+    render() {
+        // const {characterData} = this.props
+        const {characterData, removeCharacter} = this.props
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+            </table>
+        )
+        // return (
+        //     // <table>
+        //     //     <thead>
+        //     //         <th>Name</th>
+        //     //         <th>Job</th>
+        //     //     </thead>
+        //     //     <tbody>
+        //     //         <tr>
+        //     //             <td>Charlie</td>
+        //     //             <td>Janitor</td>
+        //     //         </tr>
+        //     //         <tr>
+        //     //             <td>Mac</td>
+        //     //             <td>Bouncer</td>
+        //     //         </tr>
+        //     //         <tr>
+        //     //             <td>Dee</td>
+        //     //             <td>Aspiring Actress</td>
+        //     //         </tr>
+        //     //         <tr>
+        //     //             <td>Dennis</td>
+        //     //             <td>Bartender</td>
+        //     //         </tr>
+        //     //     </tbody>
+        //     // </table>
+        //     <table>
+        //         <TableHeader />
+        //         <TableBody characterData={characterData} />
+        //     </table>
+        // )
+    }
+}
+
+export default Table
